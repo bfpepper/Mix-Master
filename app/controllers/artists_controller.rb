dafface67853dad7333429c0101a2dfed6a4e6c5
Ben Pepper <bfpepper87@gmail.com>
2016-09-03 15:28:41 -0600

    finished iteration 5

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,4 +1,5 @@
 class ArtistsController < ApplicationController
+  before_action :set_artist, only: [:show, :edit, :update, :destroy]
 
   def index
     @artists = Artist.all
@@ -18,11 +19,9 @@ class ArtistsController < ApplicationController
   end
 
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
@@ -42,6 +41,10 @@ class ArtistsController < ApplicationController
 
   private
 
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end
+  
   def artist_params
     params.require(:artist).permit(:name, :image_path)
   end
--- spec/features/user_creates_a_playlist_spec.rb
+++ spec/features/user_creates_a_playlist_spec.rb
@@ -24,13 +24,3 @@ RSpec.feature "User creates playlist" do
     end
   end
 end
-# As a user
-# Given that songs and artists exist in the database
-# When I visit the playlists path
-# And I click "New playlist"
-# And I fill in a name
-# And I select the songs for the playlist
-# And I click "Create Playlist"
-# Then I see the playlist title
-# And I see the titles of all songs in that playlist
-# And the titles should link to the individual song pages
